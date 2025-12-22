b134b80284d6ae34ec0c14c6cbdd092dce35ad0c
Facugarciar0805 <facugarciar@gmail.com>
2025-12-22 10:56:26 -0300

    Terminado ruido del acelerometro

--- main.py
+++ main.py
@@ -8,52 +8,61 @@ from sensors.acelerometro import Acelerometro
 datos = pan.read_csv("resources/prueba_simu.csv", comment="#", decimal=',')
 
 # Pasar a numpy
-tiempos = datos["Tiempo (s)"][310:365]
-altitudes = datos["Altitud (m)"][310:365]
-velocities = datos["Velocidad total (m/s)"][310:365]
-acelerations = datos["AceleraciÃ³n total (m/sÂ²)"][310:365]
+tiempos = datos["Tiempo (s)"][305:365]
+altitudes = datos["Altitud (m)"][305:365]
+velocities = datos["Velocidad total (m/s)"][305:365]
+acelerations = datos["AceleraciÃ³n total (m/sÂ²)"][305:365]
 
 
 #Sensores
 barometro = Barometro()
+acelerometro = Acelerometro()
 
 
 
 
 
 # Crear subplots compartiendo eje X
-fig, axs = plt.subplots(1, 2, sharex=True, figsize=(10, 8))
+fig, axs = plt.subplots(2, 2, sharex=True, figsize=(10, 8))
 
-lec_prueba = []
+bar_prueba = []
+acel_prueba = []
 for s,v,a in zip(altitudes,velocities,acelerations):
-    lec_prueba.append(barometro.lectura(s,v,a))
+    bar_prueba.append(barometro.lectura(s, v, a))
+    acel_prueba.append(acelerometro.lectura(a))
 
 
 
 
 # Altura
-axs[0].plot(tiempos, altitudes)
-axs[0].plot(tiempos,lec_prueba, color = "red", linestyle = "dotted")
-axs[0].set_title("Tiempo vs Altura")
-axs[0].set_ylabel("Altura [m]")
+axs[0,0].plot(tiempos, altitudes)
+axs[0,0].plot(tiempos, bar_prueba, color ="red", linestyle ="dotted")
+axs[0,0].set_title("Tiempo vs Altura")
+axs[0,0].set_ylabel("Altura [m]")
 
-errores = np.array(lec_prueba) - np.array(altitudes)
+errores = np.array(bar_prueba) - np.array(altitudes)
 
-axs[1].plot(tiempos,errores)
-axs[1].set_title("Errores barometro")
-axs[1].set_ylabel("Error de medicion [m]")
+axs[0,1].plot(tiempos,errores)
+axs[0,1].set_title("Errores barometro")
+axs[0,1].set_ylabel("Error de medicion [m]")
 
 
 """# Velocidad
 axs[1,0].plot(tiempos, velocities)
 axs[1,0].set_title("Tiempo vs Velocidad")
-axs[1,0].set_ylabel("Velocidad [m/s]")
+axs[1,0].set_ylabel("Velocidad [m/s]")"""
 
 # Aceleración
-axs[1,1].plot(tiempos, acelerations)
-axs[1,1].set_title("Tiempo vs Aceleración")
-axs[1,1].set_ylabel("Aceleración [m/s²]")
-axs[1,1].set_xlabel("Tiempo [s]")"""
+axs[1,0].plot(tiempos, acelerations)
+axs[1,0].plot(tiempos, acel_prueba, color ="red", linestyle ="dotted")
+axs[1,0].set_title("Tiempo vs Aceleración")
+axs[1,0].set_ylabel("Aceleración [m/s²]")
+axs[1,0].set_xlabel("Tiempo [s]")
+
+errores_acel = np.array(acel_prueba) -np.array(acelerations)
+axs[1,1].plot(tiempos, errores_acel)
+axs[1,1].set_title("Errores aceleracion")
+axs[1,1].set_ylabel("Aceleracion [m/s²")
 
 # Ajuste automático de espacios
 plt.tight_layout()
--- sensors/acelerometro.py
+++ sensors/acelerometro.py
@@ -1,12 +1,23 @@
 import numpy as np
 
+
 class Acelerometro:
-    def __init__(self,
-                 sigma_g=0.02,
-                 bias_g=0.02):
+    def __init__(self, sigma_g=0.02, bias_g=0.02):
         g = 9.81
-        self.sigma = sigma_g * g   # m/s²
-        self.bias  = bias_g  * g   # m/s²
+        self.sigma = sigma_g * g
+        self.bias = bias_g * g
+        self.g = g
+        # El MPU6050 se puede setear en 2, 4, 8 o 16g.
+        # Como el cohete llega a 140 m/s2 (14.3g), HAY usar el de 16g.
+        self.rango_mpu = 16 * g
 
     def lectura(self, real_ms2):
-        return real_ms2 + self.bias + np.random.normal(0, self.sigma)
+        # Ruido dinámico (vibración)
+        sigma_dinamico = self.sigma + (abs(real_ms2) * 0.1)
+        ruido = np.random.normal(0, sigma_dinamico)
+
+        medicion = real_ms2 + self.bias + ruido
+
+        # el límite físico del MPU6050 en modo 16g
+        # Si la aceleracion se pasa de los 16g el MPU devuelve uns señal plana, ya que paso su maximo de lectura
+        return np.clip(medicion, -self.rango_mpu, self.rango_mpu)
